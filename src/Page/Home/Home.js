9acdec208a102a01087c66dbf07057d264f4bb42
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-03-18 02:17:09 +0530

    added

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import Info from './Info';
+import clock from '../../assets/icons/clock.svg' 
 
 const Home = () => {
     return (
         <div>
           <Banner></Banner>
+          <Info img={Info}></Info>
         </div>
 
     );
