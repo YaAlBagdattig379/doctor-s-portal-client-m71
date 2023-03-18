e2915187c980e53d967b4d1437b42ef204001e88
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-03-18 08:58:19 +0530

    m71v5 about cardInfo dynamic css

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
 import Info from './Info';
-import clock from '../../assets/icons/clock.svg' 
 
 const Home = () => {
     return (
         <div>
           <Banner></Banner>
-          <Info img={Info}></Info>
+          <Info></Info>
         </div>
 
     );
--- src/Page/Home/InfoCard.js
+++ src/Page/Home/InfoCard.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-const InfoCard = () => {
+const InfoCard = ({img,cardTitle,bgClass}) => {
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl">
-        <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-        <div class="card-body">
-          <h2 class="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Listen</button>
+        <div class={`card lg:card-side bg-base-100 shadow-xl bg-accent ${bgClass}`}>
+          <figure className='pl-5'><img src={img} alt="Album"/></figure>
+          <div class="card-body text-white">
+            <h2 class="card-title">{cardTitle}</h2>
+            <p>Click the button to listen on Spoti why app.</p>
           </div>
-        </div>
       </div>
     );
 };
