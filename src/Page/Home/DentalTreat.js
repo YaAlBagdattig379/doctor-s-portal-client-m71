02e6ce88de1a17301dd6c64a1bf1e984d674fd57
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-03-19 23:22:45 +0530

    added

--- src/Page/Home/DentalTreat.js
+++ src/Page/Home/DentalTreat.js
@@ -5,7 +5,7 @@ import PrimaryButton from '../Shared/PrimaryButton';
 const DentalTreat = () => {
     return (
         <div className="card lg:card-side bg-base-100 shadow-xl mb-[200px]">
-          <figure className='lg:w-full'><img class='w-[320px] h-[400px]' src={dentalImg} alt="Album"/></figure>
+          <figure className='lg:w-full'><img className='w-[320px] h-[400px]' src={dentalImg} alt="Album"/></figure>
           <div className="lg:w-[80%] card-body justify-center gap-4">
           <div>
              <h2 className="card-title text-4xl">Exceptional Dental Care, on Your Terms</h2>
